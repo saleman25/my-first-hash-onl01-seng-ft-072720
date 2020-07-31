5f6ce5d0dbb102dcb69a1beb38face864a04f658
Samantha Aleman <samantha.aleman25@gmail.com>
2020-07-31 04:25:17 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -27,14 +27,10 @@ def adding
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
-  shipping_manifest["muskets"] => "2"
+  shipping_manifest["muskets"] => 2
 
 
-  # add 4 gun powder to the shipping_manifest hash below
-
-
-
-  # return the shipping_manifest hash below
+shipping_manifest["gun powder"] => 4
 
 end
 shipping_manifest
